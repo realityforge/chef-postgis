d7c83573d913847352dfac758afc615c1eaeeac0
Peter Donald <peter@realityforge.org>
2012-12-06 08:11:21 +1100

    Add initial metadata

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'postgis'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs/Configures postgis Postgresql extension'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+
+depends 'apt'
+depends 'postgresql'
+
+supports 'ubuntu'
