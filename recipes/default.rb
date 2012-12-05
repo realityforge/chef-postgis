81387936b8d972e4b12290b6f0b90609aa7567c6
Peter Donald <peter@realityforge.org>
2012-12-06 08:46:02 +1100

    Add a basic recipe that loads postgis

---
+++ recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+apt_repository 'ppa_sharpie_for-science' do
+  uri 'http://ppa.launchpad.net/sharpie/for-science/ubuntu'
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver 'keyserver.ubuntu.com'
+  key 'DAF764E2'
+  deb_src true
+end
+
+apt_repository 'ppa_sharpie_postgis-stable' do
+  uri 'http://ppa.launchpad.net/sharpie/postgis-stable/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key 'DAF764E2'
+  deb_src true
+end
+
+apt_repository 'ppa_ubuntugis_ubuntugis-unstable' do
+  uri 'http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key '314DF160'
+  deb_src true
+end
+
+package 'python-software-properties'
+package 'postgresql-9.1-postgis2'
